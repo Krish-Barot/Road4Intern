5b6ca10a521c66f5dfa1da44f56fc7cb3f8151ae
Krish Barot <HP@mac.home>
2025-09-02 18:22:55 -0400

    Make deployment public

---
+++ backend/Login_signup/api/index.js
@@ -0,0 +1,24 @@
+import connectDB from "./db.js";
+import express from 'express'
+import cors from "cors";
+import userRoutes from './routes/users.js'
+import authRoutes from './routes/auth.js'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express()
+app.use(express.json())
+ app.use(cors());
+// connecting to DB
+connectDB();
+
+// routes
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+
+const PORT = 3000
+
+
+app.listen(PORT, () =>{
+    console.log(`Server running on ${PORT}`)
+})
